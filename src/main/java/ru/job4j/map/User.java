6a04ab3bce6a32f5b78c3444b1f70a6aff7a7613
viktoryboykova <viktoryboykova@list.ru>
2021-11-12 16:52:54 +0300

    Переопределить только hashCode

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -6,22 +6,37 @@ public class User {
 
     private String name;
     private int children;
-    private Calendar birthday;
+    private Date birthday;
 
-    public User(String name, int children, Calendar birthday) {
+    public User(String name, int children, Date birthday) {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
     }
 
     public static void main(String[] args) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(1990, Calendar.DECEMBER, 20);
-        User user1 = new User("Иван", 2, calendar);
-        User user2 = new User("Иван", 2, calendar);
-        Map<User, Object> map = new HashMap<>();
-        map.put(user1, new Object());
-        map.put(user2, new Object());
-        System.out.println(map);
+        Calendar calendar2 = Calendar.getInstance();
+        calendar2.set(1990, Calendar.DECEMBER, 10);
+        Date date = Date.from(calendar2.toInstant());
+        User user3 = new User("Олег", 2, date);
+        User user4 = new User("Олег", 2, date);
+        Map<User, Object> map2 = new HashMap<>();
+        map2.put(user3, "object_1");
+        map2.put(user4, "object_2");
+        System.out.println("Переопределен только hashCode: " + map2);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", children=" + children +
+                ", birthday=" + birthday +
+                '}';
     }
 }
