a70de92d09d261284c0e108aec1e00ae0663c9f9
viktoryboykova <viktoryboykova@list.ru>
2021-11-22 12:42:16 +0300

    Тестирование IO

---
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -0,0 +1,55 @@
+package ru.job4j.io;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.*;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class AnalizyTest {
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Test
+    public void unavailableOne() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 12:30:00");
+            out.println("500 12:31:02");
+            out.println("200 12:35:30");
+            out.println("400 13:00:00");
+            out.println("200 13:01:05");
+        }
+        Analizy analizy = new Analizy();
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(rsl::append);
+        }
+        assertThat(rsl.toString(), is("12:31:02;12:35:30;13:00:00;13:01:05;"));
+    }
+
+    @Test
+    public void unavailableTwo() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 12:00:00");
+            out.println("400 12:01:00");
+            out.println("500 12:02:04");
+            out.println("200 12:03:00");
+        }
+        Analizy analizy = new Analizy();
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(rsl::append);
+        }
+        assertThat(rsl.toString(), is("12:01:00;12:03:00;"));
+    }
+}
